f283ed480cd762959ad8ee2a63e0c9d68be1ea44
Daniil <icet7523@gmail.com>
2023-06-10 00:22:12 +0300

    Добавлены функции:     проверки строк на длину,     проверки на палиндром,     извлечения чисел из входных данных Добавлены тесты на соотвествующие функции Добавлен Jest и комманда test в package.json Рефакторинг функций

--- js/functions.js
+++ js/functions.js
@@ -1,16 +1,16 @@
 const isShorterThan = (string, length) => string.length <= length;
 
-const isPalindrome = function(string) {
+const isPalindrome = (string) => {
   const palindrome = string.toLowerCase().replaceAll(' ', '');
   for (let i = 0; i < Math.floor(palindrome.length / 2); i++) {
-    if (palindrome.at(i) !== palindrome.at((palindrome.length - 1) - i)) {
+    if (palindrome.at(i) !== palindrome.at(-1 - i)) {
       return false;
     }
   }
   return true;
 };
 
-function getNumberFrom(value) {
+const getNumberFrom = (value) => {
   if (typeof value === 'number') {
     return parseInt(Math.abs(value).toString().replace('.',''),10);
   }
@@ -18,5 +18,5 @@ function getNumberFrom(value) {
 
   const res = value.match(pattern);
   return (res === null) ? NaN : parseInt(res.join(''),10);
-}
+};
 export {isShorterThan, getNumberFrom, isPalindrome};
