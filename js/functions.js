3500991c988e25e061460544bb00cb1db0673e66
Daniil <icet7523@gmail.com>
2023-06-10 21:55:03 +0300

    Заменены:nvm, npm и browser-sync на volta, pnpm и vite Добавлены: typescript, и поддержка typescript для eslint Заменены пробелы на табы Команды pnpm run: "build", "preview", "fix"

--- js/functions.js
+++ js/functions.js
@@ -1,22 +1,22 @@
 const isShorterThan = (string, length) => string.length <= length;
 
 const isPalindrome = (string) => {
-  const palindrome = string.toLowerCase().replaceAll(' ', '');
-  for (let i = 0; i < Math.floor(palindrome.length / 2); i++) {
-    if (palindrome.at(i) !== palindrome.at(-1 - i)) {
-      return false;
-    }
-  }
-  return true;
+	const palindrome = string.toLowerCase().replaceAll(' ', '');
+	for (let i = 0; i < Math.floor(palindrome.length / 2); i++) {
+		if (palindrome.at(i) !== palindrome.at(-1 - i)) {
+			return false;
+		}
+	}
+	return true;
 };
 
 const getNumberFrom = (value) => {
-  if (typeof value === 'number') {
-    return parseInt(Math.abs(value).toString().replace('.',''),10);
-  }
-  const pattern = new RegExp('[0-9]','g');
+	if (typeof value === 'number') {
+		return parseInt(Math.abs(value).toString().replace('.', ''), 10);
+	}
+	const pattern = new RegExp('[0-9]', 'g');
 
-  const res = value.match(pattern);
-  return (res === null) ? NaN : parseInt(res.join(''),10);
+	const res = value.match(pattern);
+	return (res === null) ? NaN : parseInt(res.join(''), 10);
 };
 export {isShorterThan, getNumberFrom, isPalindrome};
--- js/tests/functions.spec.js
+++ js/tests/functions.spec.js
@@ -3,45 +3,45 @@ import {describe,expect,test} from '@jest/globals';
 
 
 describe('Is string require optimal length criteria', () => {
-  const testCases = [
-    {values: ['проверяемая строка', 20], result: true},
-    {values: ['проверяемая строка', 18], result: true},
-    {values: ['проверяемая строка', 10], result: false}
-  ];
-  for (const testCase of testCases) {
-    test(`Is string "${testCase.values[0]}" shorter than ${testCase.values[1]} symbols`,
-      () => expect(isShorterThan(...testCase.values)).toStrictEqual(testCase.result));
-  }
+	const testCases = [
+		{values: ['проверяемая строка', 20], result: true},
+		{values: ['проверяемая строка', 18], result: true},
+		{values: ['проверяемая строка', 10], result: false}
+	];
+	for (const testCase of testCases) {
+		test(`Is string "${testCase.values[0]}" shorter than ${testCase.values[1]} symbols`,
+			() => expect(isShorterThan(...testCase.values)).toStrictEqual(testCase.result));
+	}
 });
 
 describe('Is string a palindrome', () => {
-  const testCases = [
-    {value: 'топот', result: true},
-    {value: 'ДовОд', result: true},
-    {value: 'Кекс', result: false},
-    {value: 'Лёша на полке клопа нашёл ', result: true}
-  ];
-  for (const testCase of testCases) {
-    test(`Is string "${testCase.value}" is a palindrome`,
-      () => expect(isPalindrome(testCase.value)).toStrictEqual(testCase.result));
-  }
+	const testCases = [
+		{value: 'топот', result: true},
+		{value: 'ДовОд', result: true},
+		{value: 'Кекс', result: false},
+		{value: 'Лёша на полке клопа нашёл ', result: true}
+	];
+	for (const testCase of testCases) {
+		test(`Is string "${testCase.value}" is a palindrome`,
+			() => expect(isPalindrome(testCase.value)).toStrictEqual(testCase.result));
+	}
 });
 
 describe('Extracting numbers from given value ', () => {
-  const testCases = [
-    {value: '2023 год', result: 2023},
-    {value: 'ECMAScript 2022', result: 2022},
-    {value: '1 кефир, 0.5 батона', result: 105},
-    {value: 'агент 007', result: 7},
-    {value: 'а я томат', result: NaN},
-    {value: 2023, result: 2023},
-    {value: -1, result: 1},
-    {value: 1.5, result: 15},
-  ];
-  for (const testCase of testCases) {
-    test(`extracting numbers from "${testCase.value}" `,
-      () => expect(getNumberFrom(testCase.value)).toStrictEqual(testCase.result));
-  }
+	const testCases = [
+		{value: '2023 год', result: 2023},
+		{value: 'ECMAScript 2022', result: 2022},
+		{value: '1 кефир, 0.5 батона', result: 105},
+		{value: 'агент 007', result: 7},
+		{value: 'а я томат', result: NaN},
+		{value: 2023, result: 2023},
+		{value: -1, result: 1},
+		{value: 1.5, result: 15},
+	];
+	for (const testCase of testCases) {
+		test(`extracting numbers from "${testCase.value}" `,
+			() => expect(getNumberFrom(testCase.value)).toStrictEqual(testCase.result));
+	}
 });
 
 
