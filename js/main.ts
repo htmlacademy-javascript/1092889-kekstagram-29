7ad541cb71047dd4d797760de4e97fc9864a8b4d
Daniil <icet7523@gmail.com>
2023-06-21 04:17:37 +0300

    Распакован getGeneratedMocks константы перееименованы в UPPERCASE и вынесены в отдельный файл сonstants
    Signed-off-by: Daniil <icet7523@gmail.com>

--- js/main.ts
+++ js/main.ts
@@ -1,48 +1,42 @@
 import {getRandomArrayElement, getRandomInt} from './utils/randomizers.ts';
 import {User, PhotoComment, Photo} from './contracts/common.ts';
 import {messages, names, descriptions} from './data/template-values.json';
+import {Constants} from './data/constants.ts';
+
 const enum Default {
-	MinId = 1,
-	MinPhotoAddress = 1,
-	MaxPhotoAddress = 25,
-	MinLikes = 15,
-	MaxLikes = 200,
-	MinComments = 0,
-	MaxComments = 30,
-	MinAvatarId = 1,
-	MaxAvatarId = 6
+	AMOUNT = 25
 }
 
-export const getGeneratedMocks = (amount: number): Photo[] => {
-	const getId = () => {
-		let id = Default.MinId;
-		return () => id++;
-	};
-	const getCommentId = getId();
-	const getPhotoId = getId();
-	const _getGeneratedUser = ():User => ({
-		avatar: `img/avatar-${getRandomInt(Default.MinAvatarId,Default.MaxAvatarId)}.svg`,
-		name: getRandomArrayElement(names)
-	});
-
-	const _getGeneratedComment = ():PhotoComment => {
-		const user:User = _getGeneratedUser();
-		return {
-			...user,
-			id: getCommentId(),
-			message: Array.from({length: getRandomInt(1, 2)}, () => getRandomArrayElement(messages)).join(' '),
-		};
-	};
-
-	const _getGeneratedPhoto = ():Photo => ({
-		comments: Array.from({length: getRandomInt(Default.MinComments,Default.MaxComments)}, _getGeneratedComment),
-		description: Array.from({length: getRandomInt(1, 6)}, () => getRandomArrayElement(descriptions)).join(' ') ,
-		id: getPhotoId(),
-		likes: getRandomInt(Default.MinLikes, Default.MaxLikes),
-		url: `photos/${getRandomInt(Default.MinPhotoAddress,Default.MaxPhotoAddress)}.jpg`
-	});
-	return Array.from({length: amount}, _getGeneratedPhoto);
+const getId = () => {
+	let id = Constants.MIN_ID;
+	return () => id++;
 };
-const amount = 25;
-getGeneratedMocks(amount);
+
+const getCommentId = getId();
+const getPhotoId = getId();
+
+const getGeneratedUser = ():User => ({
+	avatar: `img/avatar-${getRandomInt(Constants.MIN_AVATAR_ID,Constants.MAX_AVATAR_ID)}.svg`,
+	name: getRandomArrayElement(names)
+});
+
+const getGeneratedComment = ():PhotoComment => {
+	const user:User = getGeneratedUser();
+	return {
+		...user,
+		id: getCommentId(),
+		message: Array.from({length: getRandomInt(1, 2)}, () => getRandomArrayElement(messages)).join(' '),
+	};
+};
+
+const getGeneratedPhoto = ():Photo => ({
+	comments: Array.from({length: getRandomInt(Constants.MIN_COMMENTS,Constants.MAX_COMMENTS)}, getGeneratedComment),
+	description: Array.from({length: getRandomInt(1, 6)}, () => getRandomArrayElement(descriptions)).join(' ') ,
+	id: getPhotoId(),
+	likes: getRandomInt(Constants.MIN_LIKES, Constants.MAX_LIKES),
+	url: `photos/${getRandomInt(Constants.MIN_PHOTO_ADDRESS,Constants.MAX_PHOTO_ADDRESS)}.jpg`
+});
+export const getGeneratedMocks = (amount: number): Photo[] => Array.from({length: amount}, getGeneratedPhoto);
+
+getGeneratedMocks(Default.AMOUNT);
 
