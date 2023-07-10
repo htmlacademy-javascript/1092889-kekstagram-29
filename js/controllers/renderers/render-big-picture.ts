985a18cc060577a1a28d3a3130627f7329bdf3a7
Daniil <icet7523@gmail.com>
2023-07-10 18:49:27 +0300

    Update js/controllers/renderers/render-big-picture.ts
    Co-authored-by: baileys-li <47776594+baileys-li@users.noreply.github.com>

--- js/controllers/renderers/render-big-picture.ts
+++ js/controllers/renderers/render-big-picture.ts
@@ -1,7 +1,12 @@
 import {Photo} from '../../contracts/common.ts';
 
 const bigPicture = document.querySelector<HTMLElement>('.big-picture');
-if (!bigPicture) {
+const bigPictureImg = bigPicture?.querySelector<HTMLImageElement>('.big-picture__img img');
+const likesCount = bigPicture?.querySelector<HTMLSpanElement>('.likes-count');
+const commentsCount = bigPicture?.querySelector<HTMLSpanElement>('.comments-count');
+const pictureDescription = bigPicture?.querySelector<HTMLParagraphElement>('.social__caption');
+
+if (!bigPicture || !bigPictureImg || !likesCount || !commentsCount || !pictureDescription) {
 	throw new Error('Big picture not found');
 }
 const setBigPicture = ({url, likes, description}: Photo): HTMLElement=> {
