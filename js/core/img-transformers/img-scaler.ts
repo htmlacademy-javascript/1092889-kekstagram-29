da49f57b66dae92711ec6e57198dcaa7778a6bb6
Daniil <icet7523@gmail.com>
2023-07-18 08:56:01 +0300

    рефакторинг констант

--- js/core/img-transformers/img-scaler.ts
+++ js/core/img-transformers/img-scaler.ts
@@ -1,14 +1,20 @@
+const enum Default {
+	MIN_VALUE = 25,
+	MAX_VALUE = 100,
+	ADD_VALUE = 25
+}
+
 const scaleUp = (value: number) => {
-	value += 25;
-	if (value > 100) {
+	value += Default.ADD_VALUE;
+	if (value > Default.MAX_VALUE) {
 		return 1;
 	}
 	return value / 100;
 };
 
 const scaleDown = (value: number) => {
-	value -= 25;
-	if (value < 25) {
+	value -= Default.ADD_VALUE;
+	if (value < Default.MIN_VALUE) {
 		return 0.25;
 	}
 	return value / 100;
